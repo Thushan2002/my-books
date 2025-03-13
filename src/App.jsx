6a0a5b480a339671be2d7eddf8fdb45594ae295f
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-03-13 16:26:46 +0530

    feat: navbar section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
